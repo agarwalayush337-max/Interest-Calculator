1a35f4177e612dddc1013f71677f300f370dfa8c
agarwalayush337-max <agarwalayush337@gmail.com>
2025-08-25 17:55:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // --- Firebase Configuration ---
 const firebaseConfig = {
-    apiKey: "AIzaSyA7_nnw_BRziSVyjbZ-2UMxTKIKVW_K_JQ",
+    apiKey: "AIzaSyA7_nnw_BRziSVyjbZ-MxTKIKVW_K_JQ",
     authDomain: "interest-calculator-8d997.firebaseapp.com",
     projectId: "interest-calculator-8d997",
     storageBucket: "interest-calculator-8d997.appspot.com",
