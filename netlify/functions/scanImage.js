f2db335c4381b1dd331b2baf245c807f2a016bdc
agarwalayush337-max <agarwalayush337@gmail.com>
2025-08-27 01:26:14 +0530

    Update scanImage.js

--- netlify/functions/scanImage.js
+++ netlify/functions/scanImage.js
@@ -19,7 +19,7 @@ exports.handler = async function(event) {
     const accessToken = (await client.getAccessToken()).token;
 
     // --- UPDATED MODEL NAME ---
-    const MODEL_ID = 'gemini-2.0-flash-001';
+    const MODEL_ID = 'gemini-2.0-flash-lite-001';
     const apiUrl = `https://${LOCATION}-aiplatform.googleapis.com/v1/projects/${GCP_PROJECT_ID}/locations/${LOCATION}/publishers/google/models/${MODEL_ID}:generateContent`;
 
     const { image, mimeType } = JSON.parse(event.body);
