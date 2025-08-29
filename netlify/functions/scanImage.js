4726214c7015d621b14c1873adf24298a448da3d
agarwalayush337-max <agarwalayush337@gmail.com>
2025-08-30 00:56:17 +0530

    Update scanImage.js

--- netlify/functions/scanImage.js
+++ netlify/functions/scanImage.js
@@ -29,7 +29,7 @@ exports.handler = async function(event) {
         role: 'user',
         parts: [
           { inline_data: { mimeType: mimeType, data: image } },
-          { text: `From the provided image, identify all loan entries. For each entry, extract the 'LoanNo', 'Principal', and 'Date'. Return the result as a clean JSON array of objects where each object has the keys "no", "principal", and "date".Format the 'Date' field into a 'DD/MM/YYYY' string. Format the 'no' field by replacing any '.',' ','-' with a '/',And if there is nothing between Alphabet And number in 'no' field then add '/'.Do not include any text, explanations, or markdown formatting in your response, only the raw JSON array.` }
+          { text: `From the provided image, identify all loan entries. For each entry, extract the 'LoanNo', 'Principal', and 'Date'. Return the result as a clean JSON array of objects where each object has the keys "no", "principal", and "date".Format the 'Date' field into a 'DD/MM/YYYY' string. Format the 'no' field by replacing any '.',' ','-' with a '/',And if there is nothing between Alphabet And number in 'no' field then add '/'.The 'No' Field Will have a alpha numerical, and the numbers will be maximum 3, so if you see loanno Starting with Alphabet followed by 4 numbers, and if the first number is '1' then Cosider '1' as '/'. Do not include any text, explanations, or markdown formatting in your response, only the raw JSON array.` }
         ]
       }]
     };
