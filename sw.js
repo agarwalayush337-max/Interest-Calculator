c08a4f8006ef481302bac9e914f820450fc2cd42
agarwalayush337-max <agarwalayush337@gmail.com>
2025-09-18 20:13:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define a name for the cache and the files that make up the app shell
-const CACHE_NAME = 'interest-calculator-cache-v2';
+const CACHE_NAME = 'interest-calculator-cache-v1';
 const APP_SHELL_URLS = [
   '/',
   'index.html',
